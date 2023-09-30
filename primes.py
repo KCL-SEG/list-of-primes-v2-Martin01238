95f2e1087ab278a3bb11265f187227199086bf1c
Martin <martintang7567@gmail.com>
2023-09-30 19:02:59 +0100

    commit

--- primes.py
+++ primes.py
@@ -1,6 +1,13 @@
-"""List of prime numbers generator."""
+"""values of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <=0:
+        raise ValueError("No <= 0 values.")
+    values = []
+    initial = 2
+    while len(values) < number_of_primes:
+        pcheck = [initial for i in values if initial%i == 0]
+        values += [] if pcheck else [initial]
+        initial += 1
+    return values
